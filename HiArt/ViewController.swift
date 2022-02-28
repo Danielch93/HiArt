02748a97cb49c40b2df72b92278b7a72b6197634
Daniel Charry <dacharry93@gmail.com>
2022-02-27 23:25:30 -0500

    Initial Commit

---
+++ HiArt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiArt
+//
+//  Created by Daniel Charry on 27/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
