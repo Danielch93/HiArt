b451fc8bf3679dff11403bcd74f92aabd2f98ede
Daniel Charry <dacharry93@gmail.com>
2022-04-18 20:55:50 -0500

    remove print

--- HiArt/UseCases/Common/BaseView/BaseView.swift
+++ HiArt/UseCases/Common/BaseView/BaseView.swift
@@ -20,7 +20,6 @@ class BaseView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
-        print("here!")
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
