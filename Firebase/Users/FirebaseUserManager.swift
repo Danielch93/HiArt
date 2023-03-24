92eb430758a0b4b73a08886869122609f9939a61
Daniel Charry <dacharry93@gmail.com>
2023-03-23 22:03:58 -0500

    Create FirebaseAuth and FirebaseUserManager

---
+++ Firebase/Users/FirebaseUserManager.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseUserManager.swift
+//  HiArt
+//
+//  Created by Daniel Charry on 19/03/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+struct FirebaseUserManager {
+    static func createUser() {
+        Auth.auth().createUser(withEmail: "daniel@correo.com", password: "123456") { (result, error) in
+            print("User created")
+        }
+    }
+}
