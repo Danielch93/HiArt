5b818a9ff4031fd004e9bcac9655bde5451c9c1d
Daniel Charry <dacharry93@gmail.com>
2023-09-13 23:29:51 -0500

    Update pre-commit.bash

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo “Running pre-commit hook”
-xcodebuild -quiet -workspace HiArt.xcworkspace -scheme HiArt -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.4' test
+xcodebuild -quiet -workspace HiArt.xcworkspace -scheme HiArt -destination 'platform=iOS Simulator,name=iPhone 8,OS=16.4' test
 if [ $? -ne 0 ]; then
  echo “Tests must pass before commit!”
  exit 1
